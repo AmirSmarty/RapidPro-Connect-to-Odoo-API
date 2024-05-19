112cd4609f0dd738a63cf79a53f99f7c62b9bc0e
Emir <graciasmama@gmail.com>
2024-05-19 23:10:01 +0000

    Module Odoo for tracking rumors from Community
    Signed-off-by: Emir <graciasmama@gmail.com>

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -8,7 +8,7 @@ class Rumors(http.Controller):
     @http.route('/rumor/alert/add', type='http', auth='none', methods=['GET'], csrf=False)
 
     def add_alert(self, village=None, description=None, cvac_name=None, cvac_tel=None, icp_name=None, icp_tel=None,
-                  region=None, district=None, structure=None ** kw):
+                  region=None, district=None, structure=None, **kw):
         # Valider les paramètres
         if not village or not description or not cvac_name or not cvac_tel or not icp_name or not icp_tel or not region or not district or not structure:
             return _("Paramètres manquants")
